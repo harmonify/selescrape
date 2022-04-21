2d1da8840080a20a66318b57031beabed6015042
Wendy Surya Wijaya <84518752+harmonify@users.noreply.github.com>
2022-04-21 19:06:31 +0700

    add Medium trending links scraper

---
+++ src/lib/medium_trending_links_scraper.py
@@ -0,0 +1,55 @@
+from __future__ import annotations
+import os
+import re
+from typing import List
+from bs4 import BeautifulSoup
+from lib import BaseScraper
+
+
+class MediumTrendingLinksScraper(BaseScraper):
+    def __init__(self, url: str, file_name: str = "", config_name: str = "", wait_for: str | None = None, wait_timeout: int = 0) -> None:
+        super().__init__(url, file_name, config_name, wait_for, wait_timeout)
+        self.trending_links = []
+
+    def scrape_trending_links(self) -> List[str]:
+        """
+        Scrape trending article links from the `html` attribute and return the response.
+        It is recommended to use the `fetch_html` method and check the `html`
+        attribute before scraping the html.
+        """
+        if len(self.html) == 0:
+            print("No html to scrape")
+            return []
+
+        soup = BeautifulSoup(self.html, 'html.parser')
+        # find all links in trending posts
+        # implementation: find all article links in .pw-trending-post .am.cy and the second .hm.y
+        print("Scraping trending links")
+        for article in soup.find_all(class_="pw-trending-post"):
+            link = article.find(class_="am cy").find_all(
+                class_="hm y")[1].find("a").get("href")
+            if re.match(r'^/[^@]', link):
+                link = "https://medium.com" + link
+            self.trending_links.append(link)
+        print(f"Done. Found {len(self.trending_links)} links")
+        return self.trending_links
+
+    def save_trending_links(self, output_file_name=None) -> None:
+        """
+        Save the trending article links to a file. It is recommended to use the
+        `scrape_trending_links` method and check the `trending_links` attribute
+        before saving the links.
+        """
+        if len(self.trending_links) == 0:
+            print("No trending links to save")
+            return
+
+        output_path = os.path.join(
+            self.config["output_dir_path"], output_file_name or self.file_name) + ".txt"
+
+        print(
+            f"Saving {len(self.trending_links)} trending links to {output_path}")
+        with open(f"{output_path}", 'w') as f:
+            for link in self.trending_links:
+                f.write(f"{link}\n")
+        print("Done")
---
+++ src/scrape_medium_trending_links.py
@@ -0,0 +1,44 @@
+import sys
+from lib import MediumTrendingLinksScraper
+
+
+def main(args=None):
+    """
+    Main function.
+
+    Command line syntax:
+    python scrape_medium_trending_links.py [file_name] [config_name] [wait_for] [wait_timeout]
+
+    <> are required arguments
+    [] are optional arguments
+    """
+    try:
+        # initialize scraper
+        url = "https://www.medium.com"
+        file_name = args[1] if len(args) >= 2 else input(
+            "Enter file name (default is auto-generated): ")
+        config_name = args[2] if len(args) >= 3 else input(
+            "Enter config name (default: selescrape.json): ")
+        wait_for = args[3] if len(args) >= 4 else input(
+            "Enter wait for selector when fetching html (default: None): ")
+        wait_timeout = args[4] if len(args) >= 5 else input(
+            "Enter wait timeout when fetching html (default: 0): ")
+        scraper = MediumTrendingLinksScraper(url, file_name, config_name,
+                                             wait_for, wait_timeout)
+        # run scraper
+        scraper.fetch_html()
+        scraper.scrape_trending_links()
+        scraper.save_trending_links()
+
+        # print out trending links
+        print("\n==============================")
+        print("Trending article links:")
+        for link in scraper.trending_links:
+            print(link)
+        print("==============================\n")
+    except KeyboardInterrupt:
+        print("\nExiting...")
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
