2f50af3d2d363d860a298a22a29df2053efc5ea2
Wendy Surya Wijaya <84518752+harmonify@users.noreply.github.com>
2022-04-22 12:24:58 +0700

    add validation

--- src/lib/utilities/construct_file_name_from_url.py
+++ src/lib/utilities/construct_file_name_from_url.py
@@ -6,10 +6,14 @@ def construct_file_name_from_url(url: str) -> str:
     """
     Construct a file name from the given url.
     """
+    now = datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
+    if not url:
+        return now
+
     parsed_url = urlparse(url)
     return "".join([
         parsed_url.netloc,
         parsed_url.path.replace("/", "_"),
         parsed_url.query,
         parsed_url.fragment
-    ]) + datetime.datetime.now().strftime("_%Y_%m_%d_%H_%M_%S")
+    ]) + f"_{now}"
